c848647da26fbc54c0527934e0a8875eed947b3a
hyennin <chm050202@gmail.com>
2022-11-29 14:36:55 +0900

    RePermutation 실습

---
+++ ch10/ch10/RePermutation1/RePermutation1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define R 3
+
+int b[R] = { 0, };
+
+void RePerPrint() {
+	for (int i = 0; i < R; i++) printf("%5d", b[i]);
+	printf("\n");
+}
+
+void RePermutation(int a[], int n, int r) {
+	int i;
+
+	if (r >= R) RePerPrint();
+	else for (i = 0; i < n; i++) {
+		b[r] = a[i];
+		RePermutation(a, n, r + 1);
+	}
+}
+
+int main(void) {
+	int a[] = { 1, 2, 3, 4, 5 };
+	int n = sizeof(a) / sizeof(int);
+
+	RePermutation(a, n, 0);
+	
+	return 0;
+}
